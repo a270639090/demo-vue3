b0f75169ddf8db1b0f6cdfd7258f33b7982d63cb
a270639090 <69082802+a270639090@users.noreply.github.com>
2023-01-30 15:08:31 +0800

    引入vueuse库

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 @click="getMouse">This is an about page</h1>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { watch } from '@vue/runtime-core'
+import { useMouse, usePreferredDark, useTitle } from '@vueuse/core'
+
+const { x, y} = useMouse()
+const getMouse = () => {
+  console.log(x.value, y.value)
+}
+
+const isDark = usePreferredDark()
+
+console.log('isDark',isDark.value)
+
+const title = useTitle('Set title')
+
+watch(isDark, () => {
+  title.value = isDark.value ? '🌙 Good evening!' : '☀️ Good morning!'
+})
+</script>
+
+<style lang="less">
+h1 {
+  width: 100%;
+  height: 600px;
+}
+</style>
